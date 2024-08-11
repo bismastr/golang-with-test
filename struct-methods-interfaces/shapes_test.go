7d1485787b9ba4cf2aec2c47e064954127958ca6
bismastr <bismasatrian@gmail.com>
2024-08-11 14:53:47 +0700

    struct methods interface

---
+++ struct-methods-interfaces/shapes_test.go
@@ -0,0 +1,52 @@
+package struct_methods_interfaces
+
+import (
+	"testing"
+)
+
+func TestParimeter(t *testing.T) {
+	actual := Perimeter(Rectangle{10.0, 10.0})
+	expected := 40.0
+
+	if actual != expected {
+		t.Errorf("got %.2f want %.2f", actual, expected)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		}
+	}
+}
