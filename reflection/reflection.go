dfc760c2439856490e6f831b49257eafbb59756b
bismastr <bismasatrian@gmail.com>
2024-08-15 10:50:25 +0700

    reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,40 @@
+package main
+
+import "reflect"
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
